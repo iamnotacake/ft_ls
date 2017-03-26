77395a978cbb437df0756199e3f32342560d6515
Anatoliy Lischynsky <alischyn@student.unit.ua>
2017-03-26 18:55:35 +0300

    Add padding for owner group

--- ls_print_prepare.c
+++ ls_print_prepare.c
@@ -6,7 +6,7 @@
 /*   By: alischyn <alischyn@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/26 18:39:33 by alischyn          #+#    #+#             */
-/*   Updated: 2017/03/26 18:42:45 by alischyn         ###   ########.fr       */
+/*   Updated: 2017/03/26 18:53:59 by alischyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,19 @@ void			ls_print_list_prepare_owner_name_pad(t_list *list)
 	i = -1;
 	while (++i < list->items.length)
 		VEC_NTH(&list->items, i, t_fileinfo *)->fmt_owner_name_pad = padding;
+}
+
+void			ls_print_list_prepare_owner_group_pad(t_list *list)
+{
+	int			i;
+	int			padding;
+
+	padding = 0;
+	i = -1;
+	while (++i < list->items.length)
+		padding = ft_max(padding,
+			strlen(VEC_NTH(&list->items, i, t_fileinfo *)->fmt_owner_group));
+	i = -1;
+	while (++i < list->items.length)
+		VEC_NTH(&list->items, i, t_fileinfo *)->fmt_owner_group_pad = padding;
 }
