324ec41e36a915392c3d4596cd91ac0d1a1b814b
Anatoliy Lischynsky <alischyn@student.unit.ua>
2017-03-29 17:44:36 +0300

    Completely fixed mtime formatter

--- fileinfo_format_mtime.c
+++ fileinfo_format_mtime.c
@@ -6,7 +6,7 @@
 /*   By: alischyn <alischyn@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/24 19:20:08 by alischyn          #+#    #+#             */
-/*   Updated: 2017/03/28 18:00:23 by alischyn         ###   ########.fr       */
+/*   Updated: 2017/03/29 17:43:43 by alischyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void			fileinfo_format_mtime(t_fileinfo *fi)
 {
-	char		time_str[64];
+	char		tm_now[32];
+	time_t		time_now;
+	char		tm_file[32];
 	char		*tm;
-	time_t		curr_time;
+	bool		same_year;
 
-	time(&curr_time);
-	tm = ctime(&curr_time);
-	__builtin_strcpy(time_str, tm);
+	time(&time_now);
+	tm = ctime(&time_now);
+	__builtin_strcpy(tm_now, tm);
 	tm = ctime(&fi->stat.st_mtime);
-	__builtin_strcpy(time_str + 32, tm);
+	__builtin_strcpy(tm_file, tm);
+	same_year = time_now - fi->stat.st_mtime < 60 * 60 * 24 * 182;
 	ft_sprintf(fi->fmt_mtime, "%.7s%.5s ",
-				time_str + 32 + 4,
-				__builtin_strncmp(time_str + 20, time_str + 32 + 20, 4) == 0
-					? time_str + 32 + 11
-					: time_str + 32 + 19);
+				tm_file + 4, same_year ? tm_file + 11 : tm_file + 19);
 }
